66f2e0b7caec2dd5a8e28ea70fbda9764e46f8c0
Felipe Contreras <felipe.contreras@gmail.com>
2024-04-16 18:15:56 -0600

    test: update to sharness 1.2.0
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- t/test-lib.sh
+++ t/test-lib.sh
@@ -8,3 +8,12 @@ GIT_COMMITTER_EMAIL=committer@example.com
 GIT_COMMITTER_NAME='C O Mitter'
 export GIT_AUTHOR_EMAIL GIT_AUTHOR_NAME
 export GIT_COMMITTER_EMAIL GIT_COMMITTER_NAME
+
+test_must_fail() {
+        "$@"
+	test $? != 0
+}
+
+test_cmp() {
+	diff -u "$@"
+}
